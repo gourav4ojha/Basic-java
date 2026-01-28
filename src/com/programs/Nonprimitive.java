f6c69848c8658000242c7fd8aa3603522a485521
Gourav Ojha <gourav@Gouravs-MacBook-Air.local>
2026-01-28 13:17:03 +0530

    token,typecasting,Wrapper

---
+++ src/com/programs/Nonprimitive.java
@@ -0,0 +1,20 @@
+package com.programs;
+
+import java.util.Arrays;
+
+public class Nonprimitive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = new String("welcome");
+		System.out.println(str);
+		double [] arr = {10,30.6,40};
+		for(double i: arr) {
+			System.out.println(i);
+		}
+		System.out.println(arr); // will give object
+		System.out.println(Arrays.toString(arr));
+
+	}
+
+}
---
+++ src/com/programs/WrapperClass.java
@@ -0,0 +1,19 @@
+package com.programs;
+
+public class WrapperClass {
+
+	public static void main(String[] args) {
+		
+//		Autoboxing
+		int a =10;
+		Integer b = a;
+		System.out.println(b); //10
+		
+//		Unboxing
+		Integer c=20;
+		int d=c;
+		System.out.println(d); //20;
+
+	}
+
+}
